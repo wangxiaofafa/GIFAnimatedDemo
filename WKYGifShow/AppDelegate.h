c2997d0e62172e4acb93428a09d13e789a555782
wangkuiyi <wangkuiyixh@163.com>
2016-05-16 17:22:01 +0800

    Initial Commit

---
+++ WKYGifShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKYGifShow
+//
+//  Created by fa on 16/5/16.
+//  Copyright © 2016年 fa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKYGifShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKYGifShow
+//
+//  Created by fa on 16/5/16.
+//  Copyright © 2016年 fa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
